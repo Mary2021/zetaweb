14af0f348703970d62e770eabe253c166eefe3f6
Mary <mary.pommer@tptlive.ee>
2022-10-26 17:34:59 +0300

    add hover action to project images

--- components/About.jsx
+++ components/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
     return (
         <div className='max-w-[1240px] m-auto p-4 h-screen'>
             {/* <h1 className='text-2xl font-bold text-center p-4'>Me teeme, mida teil on vaja!</h1> */}
-            <p className='py-5 text-xl p-4'>Analüüsime teie probleeme, vajadusi ja pakume teile lahenduse, mis aitab teie igapäevaseid probleeme lahendada või leevendada. Nii säästame teie aega ja raha.</p>
+            <p className='py-5 text-xl p-4 first-letter:text-7xl'>Analüüsime teie probleeme, vajadusi ja pakume teile lahenduse, mis aitab teie igapäevaseid probleeme lahendada või leevendada. Nii säästame teie aega ja raha.</p>
         </div>
     )
 }
