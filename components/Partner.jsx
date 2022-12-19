be457e7514090e7a32a7b280b987e5df23d9aa00
Mary <mary.pommer@tptlive.ee>
2022-12-19 15:50:59 +0200

    fix all img urls

--- components/Partner.jsx
+++ components/Partner.jsx
@@ -5,10 +5,10 @@ const Partner = () => {
     <div className='max-w-[1240px] m-auto p-4 h-screen'>
         <h1 className='text-2xl font-bold text-center p-4 '></h1>
             <div className='grid sm:grid-cols-1 md:grid-cols-2 gap-2 justify-items-center'>
-                <img src='/um.png' alt='United Motors logo'></img>
-                <img src='/MS_logo.png' alt='Microsofti logo'></img>
-                <img src='/lenovo.png' alt='Lenovo logo'></img>
-                <img src='/laptopid.png' alt='Laptopid.ee logo'></img>
+                <img src='um.png' alt='United Motors logo'></img>
+                <img src='MS_logo.png' alt='Microsofti logo'></img>
+                <img src='lenovo.png' alt='Lenovo logo'></img>
+                <img src='laptopid.png' alt='Laptopid.ee logo'></img>
             </div>
     </div>
   )
