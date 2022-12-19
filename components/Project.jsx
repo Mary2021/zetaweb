e969eb7914f0085460d532d2eb38a2460af5513e
Mary <mary.pommer@tptlive.ee>
2022-12-19 15:19:00 +0200

    process env public url

--- components/Project.jsx
+++ components/Project.jsx
@@ -5,7 +5,7 @@ const Project = () => {
         <div className='max-w-[1240px] m-auto p-4 h-screen'>
             <h1 className='text-2xl font-bold text-center p-4'></h1>
         <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2'>
-            <img className='rounded border scale-100 sm:disabled md:hover:scale-150 hover:z-40 ease-in duration-500' src='public/dbhpthumb.png' alt='DB-HP'></img>
+            <img className='rounded border scale-100 sm:disabled md:hover:scale-150 hover:z-40 ease-in duration-500' src={process.env.PUBLIC_URL + '/dbhpthumb.png'} alt='DB-HP'></img>
                 <img className='rounded border scale-100 sm:disabled md:hover:scale-150 hover:z-40 ease-in duration-500' src='/biggerthumb.png' alt='DB-HP'></img>
                 <img className='rounded border scale-100 sm:disabled md:hover:scale-150 hover:z-40 ease-in duration-500' src='/ebrexthumb.png' alt='DB-HP'></img>
                 <img className='rounded border scale-100 sm:disabled md:hover:scale-150 hover:z-40 ease-in duration-500' src='/digimajutusthumb.png' alt='DB-HP'></img>
