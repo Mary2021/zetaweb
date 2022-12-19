ec71e005222e5b77bdf37d2d63e971b3a7c85a54
Mary <mary.pommer@tptlive.ee>
2022-12-19 11:55:44 +0200

    fix hover on mobile

--- components/Project.jsx
+++ components/Project.jsx
@@ -5,15 +5,15 @@ const Project = () => {
         <div className='max-w-[1240px] m-auto p-4 h-screen'>
             <h1 className='text-2xl font-bold text-center p-4'></h1>
         <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2'>
-            <img className='rounded border scale-100 hover:scale-150 ease-in duration-500' src='/dbhpthumb.png' alt='DB-HP'></img>
-                <img className='rounded border scale-100 hover:scale-150 ease-in duration-500' src='/biggerthumb.png' alt='DB-HP'></img>
-                <img className='rounded border scale-100 hover:scale-150 ease-in duration-500' src='/ebrexthumb.png' alt='DB-HP'></img>
-                <img className='rounded border scale-100 hover:scale-150 ease-in duration-500' src='/digimajutusthumb.png' alt='DB-HP'></img>
-                <img className='rounded border scale-100 hover:scale-150 ease-in duration-500' src='/kodakodathumb.png' alt='DB-HP'></img>
-                <img className='rounded border scale-100 hover:scale-150 ease-in duration-500' src='/teammoisarhumb.png' alt='DB-HP'></img>
-                <img className='rounded border scale-100 hover:scale-150 ease-in duration-500' src='/mobothumb.png' alt='DB-HP'></img>
-                <img className='rounded border scale-100 hover:scale-150 ease-in duration-500' src='/laptopidthumb.png' alt='DB-HP'></img>
-                <img className='rounded border scale-100 hover:scale-150 ease-in duration-500' src='/mulldigitalthumb.png' alt='DB-HP'></img>
+            <img className='rounded border scale-100 sm:disabled md:hover:scale-150 hover:z-40 ease-in duration-500' src='/dbhpthumb.png' alt='DB-HP'></img>
+                <img className='rounded border scale-100 sm:disabled md:hover:scale-150 hover:z-40 ease-in duration-500' src='/biggerthumb.png' alt='DB-HP'></img>
+                <img className='rounded border scale-100 sm:disabled md:hover:scale-150 hover:z-40 ease-in duration-500' src='/ebrexthumb.png' alt='DB-HP'></img>
+                <img className='rounded border scale-100 sm:disabled md:hover:scale-150 hover:z-40 ease-in duration-500' src='/digimajutusthumb.png' alt='DB-HP'></img>
+                <img className='rounded border scale-100 sm:disabled md:hover:scale-150 hover:z-40 ease-in duration-500' src='/kodakodathumb.png' alt='DB-HP'></img>
+                <img className='rounded border scale-100 sm:disabled md:hover:scale-150 hover:z-40 ease-in duration-500' src='/teammoisarhumb.png' alt='DB-HP'></img>
+                <img className='rounded border scale-100 sm:disabled md:hover:scale-150 hover:z-40 ease-in duration-500' src='/mobothumb.png' alt='DB-HP'></img>
+                <img className='rounded border scale-100 sm:disabled md:hover:scale-150 hover:z-40 ease-in duration-500' src='/laptopidthumb.png' alt='DB-HP'></img>
+                <img className='rounded border scale-100 sm:disabled md:hover:scale-150 hover:z-40 ease-in duration-500' src='/mulldigitalthumb.png' alt='DB-HP'></img>
         </div>
       </div >
     )
